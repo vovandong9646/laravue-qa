d1f681463711e72bf08a49602823091bf0aaf6fd
vovandong14i1 <vovandong14i1@gmail.com>
2019-11-06 23:03:19 +0700

    add votes, anwser, view counter to question item in view

--- app/User.php
+++ app/User.php
@@ -8,36 +8,43 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
-    public function questions() {
-      return $this->hasMany(Question::class);
-    }
+  public function questions()
+  {
+    return $this->hasMany(Question::class);
+  }
+
+  public function getUrlAttribute()
+  {
+    return "#";
+//    return route("users.show", $this->id);
+  }
 }
