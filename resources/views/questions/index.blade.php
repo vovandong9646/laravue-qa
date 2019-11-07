4f8056b30ee1fa3e8bd182cd3055f6f8d4f40b9b
vovandong14i1 <vovandong14i1@gmail.com>
2019-11-07 22:05:05 +0700

    add logic create question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -15,6 +15,7 @@
                     </div>
 
                     <div class="card-body">
+                        @include ('layouts._message')
                         @foreach($questions as $question)
                             <div class="media">
                                 <div class="d-flex flex-column counters">
